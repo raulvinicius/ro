91abc0c41382561e8b1a51807d9804d8962784de
Raissa Costa <raissacostaesilva@gmail.com>
2015-05-08 04:11:33 -0300

    frontend home 50 porcento

--- wp-content/themes/raony/frontend/index.php
+++ wp-content/themes/raony/frontend/index.php
@@ -7,16 +7,77 @@
 
 		<section id="apresentacao">
 			<div class="container-fluid">
+
 				<div class="row">
+				<div id="bg-logo">
+					<div id="cols" class="col-sm-12">
+						<figure><img src="<?php bloginfo('template_url') ?>/img/raonyoliveira-logo.png"></figure>
+					</div>
+				</div>
+				</div>
+
+			</div>
+		</section>
+
+		<section id="servicos">
+			<div class="container-fluid">
+
+				<div id="intro" class="row">
+
+					<div class="col-sm-12">
+						<h2>Lembra, aquela sensação de carro novo?
+						<span>Nós também adoramos!</span></h2>
+					</div>
+
 					<div class="col-sm-4 col-sm-offset-4">
-						<figure><img src="?php bloginfo('template_url') ?>/img/raonyoliveira-logo.png"></figure>
-						<h1>hey!</h1>
+						<p>E pensando nisso, oferecemos os melhores serviços para trazer de volta, pra você, aquela sensação, ou se for o caso, não deixá-la ir embora.</p>
+					</div>
+
+				</div>
+
+				<div class="row">
+
+					<div id="martelinho" class="col-sm-4">
+						<h3>
+							<i><img src="<?php bloginfo('template_url') ?>/img/raonyoliveira-icon-martelinho.png"></i>
+							Martelinho de Ouro
+						</h3>
+
+						<p>Restauração artesanal, lataria original.</p>
+
+						<div class="linha-divisao"></div>
+
+						<a class="veja-mais" href="#"><span>Conheça melhor</span></a>
 
 					</div>
 
+					<div id="polimento" class="col-sm-4">
+						<h3>
+							<i><img src="<?php bloginfo('template_url') ?>/img/raonyoliveira-icon-polimento.png"></i>
+							Polimento
+						</h3>
 
+						<p>Pintura tão viva, que quase respira.</p>
 
+						<div class="linha-divisao"></div>
 
+						<a class="veja-mais" href="#"><span>Conheça melhor</span></a>
+
+					</div>
+
+					<div id="estetica" class="col-sm-4">
+						<h3>
+							<i><img src="<?php bloginfo('template_url') ?>/img/raonyoliveira-icon-estetica.png"></i>
+							Estética automotiva
+						</h3>
+
+						<p>Está tudo nos detalhes.</p>
+
+						<div class="linha-divisao"></div>
+
+						<a class="veja-mais" href="#"><span>Conheça melhor</span></a>
+
+					</div>
 				</div>
 			</div>
 		</section>
