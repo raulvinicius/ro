62bd2950039bd6b3c41b68b0fac95ba2fb8eb890
Raissa Costa <raissacostaesilva@gmail.com>
2015-05-19 17:29:50 -0300

    frontend home-depoimentos

--- wp-content/themes/raony/frontend/index.php
+++ wp-content/themes/raony/frontend/index.php
@@ -79,6 +79,47 @@
 
 					</div>
 				</div>
+
+			</div>
+		</section>
+
+		<section id="depoimentos">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3">
+						<h2>
+							<i><img src="<?php bloginfo('template_url') ?>/img/raonyoliveira-icon-aspas.png"></i>
+							Clientes felizes
+						</h2>
+					</div>
+				</div>
+
+				<div class="row">
+					<div id="seta-left" class="col-xs-2 col-sm-offset-1 col-sm-1 col-md-offset-2 col-md-1 col-lg-offset-3 col-lg-1">
+						<button><img src="<?php bloginfo('template_url') ?>/img/raonyoliveira-icon-seta-depoimentos-left.png"></button>
+					</div>
+
+					<div class="col-xs-8 col-sm-8 col-md-6 col-lg-4">
+						<blockquote>
+							<p>
+								Do mesmo modo, a competitividade nas transaações comerciais acarreta
+								um processo de reformulação e modernização dos níveis de motivação
+								departamental.
+							</p>
+
+							<footer>
+								Caren Campbell
+								<span>Arquiteta</span>
+							</footer>
+						</blockquote>
+					</div>
+					
+					<div id="seta-right" class="col-xs-2 col-sm-1 col-md-1 col-lg-1">
+						<button><img src="<?php bloginfo('template_url') ?>/img/raonyoliveira-icon-seta-depoimentos-right.png"></button>
+					</div>
+				</div>
+
 			</div>
 		</section>
 
